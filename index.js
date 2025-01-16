512cd201f43866be238d05a3fac3a29ca28e0d19
viniciusvilelaa <ninobez@icloud.com>
2025-01-15 21:12:31 -0300

    Adicinada func de verificacao do email e senha

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const user = document.getElementById('user');
 const password = document.getElementById('password');
 const btnLogin = document.getElementById('login_button');
-const span =  document.querySelectorAll('span-error');
-const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+//const span =  document.querySelectorAll('span-error');
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
 btnLogin.addEventListener("click", validateLogin);
 
 
 function validatePassword(){
     
-    let passwordValue =  user.value.length
+    let passwordValue =  password.value.length
     if(passwordValue < 6){
         alert('Senha inválida')
+        return false
     }else{
         alert('Senha válida')
+        return true
     }
 }
 
@@ -26,10 +28,31 @@ function validateEmail(){
     }
 }
 
+function error() {
+    let passwordValue = password.value.length;
+    let email = user.value
+
+    console.log("Senha:", passwordValue, "Email válido?", email);
+
+    if (passwordValue > 6 && !emailRegex.test(email)) {
+        //EMAIL INVALIDO E SENHA VALIDA
+    } else if (passwordValue <= 6 && emailRegex.test(email)) {
+        //EMAIL VALIDO E SENHA INVALIDA
+    } else if (passwordValue > 6 && emailRegex.test(email)) {
+        //LOGIN VALIDO
+    } else {
+        //LOGIN INVÁLIDO
+    }
+}
+
+
 function validateLogin(){
     validateEmail()
     validatePassword()
+    error()
 }
+
+
 
 
 
